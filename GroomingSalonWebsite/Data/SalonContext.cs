ccbd282500940ea216fccbd2ea3875a6ad8cc7f1
Ryder <rdeback@gmail.com>
2021-09-30 16:09:15 -0700

    Created DB initial context

---
+++ GroomingSalonWebsite/Data/SalonContext.cs
@@ -0,0 +1,18 @@
+﻿using GroomingSalonWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroomingSalonWebsite.Data
+{
+    public class SalonContext : DbContext
+    {
+        public SalonContext(DbContextOptions<SalonContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
