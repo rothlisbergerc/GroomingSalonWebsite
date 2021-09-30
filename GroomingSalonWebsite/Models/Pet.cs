6d3ff9c53d63e81a18a2e0e32abce9029b7aed3c
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2021-09-30 16:11:22 -0700

    Issue#03 setup db (#14)
    * Created pet and customer classes
    
    * Created DB initial context
    
    * Added all DB context parts and make initial migration

---
+++ GroomingSalonWebsite/Models/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroomingSalonWebsite.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+
+        public String Name { get; set; }
+
+        public String Breed { get; set; }
+
+        //Made a float because most animals won't live past roughly 25-30 
+        public float Age { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Breed;
+        }
+    }
+}
