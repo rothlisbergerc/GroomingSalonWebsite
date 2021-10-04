062689d7a276be06793d9f5a6beaf5099fd9cd63
Cheryl Rothlisberger <rothlisbergercheryl@gmail.com>
2021-10-04 11:21:08 -0700

    Created Appointment Model

---
+++ GroomingSalonWebsite/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroomingSalonWebsite.Models
+{
+    public class Appointment
+    {
+        public  int AppointmentId { get; set; }
+
+        public DateTime AppointmentDate { get; set; }
+
+        public String ApptPhoneNumber { get; set; }
+
+        public string ApptName { get; set; }
+
+        public string ApptPetName { get; set; }
+
+        public override string ToString()
+        {
+            return AppointmentDate + " for " + ApptName + " and" + ApptPetName ;
+        }
+    }
+}
