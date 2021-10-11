66e055a51098ea9628255e6fd41fdcff01a33b30
Cheryl Rothlisberger <RothlisbergerCheryl@gmail.com>
2021-10-11 10:34:54 -0700

    Created Contact.cs model

---
+++ GroomingSalonWebsite/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroomingSalonWebsite.Models
+{
+    public class Contact
+    {
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string subject { get; set; }
+
+        public string message { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {name} Email: {email} subject: {subject} message: {message}";
+        }
+
+    }
+}
