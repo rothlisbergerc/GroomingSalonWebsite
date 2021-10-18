1e00f6c264f0088dd03eb806378b90eb74339b18
Ryder <rdeback@gmail.com>
2021-10-18 08:14:54 -0700

    Created the DB call for the list of messages.

--- GroomingSalonWebsite/Controllers/ContactUsController.cs
+++ GroomingSalonWebsite/Controllers/ContactUsController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GroomingSalonWebsite.Models;
+using GroomingSalonWebsite.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GroomingSalonWebsite.Controllers
 {
     public class ContactUsController : Controller
     {
-        public IActionResult Index()
+        private readonly SalonContext _context;
+        public ContactUsController(SalonContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<ContactUs> Messages = await SalonDB.getContactMessageAsync(_context);
+            return View(Messages);
         }
     }
 }
