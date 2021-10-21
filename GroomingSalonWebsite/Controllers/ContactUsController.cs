d9030d8684022a9d1f3cdb4331a1232e80f782ff
Ryder <rdeback@gmail.com>
2021-10-20 17:59:15 -0700

    Created the delete in the ContactUsController.

--- GroomingSalonWebsite/Controllers/ContactUsController.cs
+++ GroomingSalonWebsite/Controllers/ContactUsController.cs
@@ -38,6 +38,23 @@ namespace GroomingSalonWebsite.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            ContactUs cm = await SalonDB.getMessageAsync(_context, id);
+            return View(cm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            ContactUs cm = await SalonDB.getMessageAsync(_context, id);
+            _context.Entry(cm).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index","Home");
+        }
         /*
         [HttpGet]
         public IActionResult Add()
