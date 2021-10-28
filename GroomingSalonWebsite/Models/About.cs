ce7f4acc9eec678e34316e03c62766b2b73d0b24
Cheryl Rothlisberger <RothlisbergerCheryl@gmail.com>
2021-10-28 12:27:10 -0700

    created About model

---
+++ GroomingSalonWebsite/Models/About.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroomingSalonWebsite.Models
+{
+    public class About
+    {
+        public String aboutText { get; set; }
+    }
+}
