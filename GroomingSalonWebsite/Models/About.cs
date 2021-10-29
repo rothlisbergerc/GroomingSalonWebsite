360cc8b9b3be783005a95321a647c96b4fdcde3f
Cheryl Rothlisberger <RothlisbergerCheryl@gmail.com>
2021-10-29 16:40:07 -0700

    Added Keyless data annotation to the about model

--- GroomingSalonWebsite/Models/About.cs
+++ GroomingSalonWebsite/Models/About.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GroomingSalonWebsite.Models
 {
+    [Keyless]
     public class About
     {
         public String aboutText { get; set; }
