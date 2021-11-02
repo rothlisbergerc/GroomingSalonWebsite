c3476a60a182ec151946fcd86516fe27633b837c
Ryder <rdeback@gmail.com>
2021-11-02 16:11:12 -0700

    Migrated and restructured the appointment class

--- GroomingSalonWebsite/Models/Appointment.cs
+++ GroomingSalonWebsite/Models/Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,41 @@ namespace GroomingSalonWebsite.Models
 {
     public class Appointment
     {
+        [Key]
+        public int AppointmentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PetName { get; set; }
+
+        public string PetBreed { get; set; }
+
+        public DateTime PetBirthday { get; set; }
+
+        public int PetWeight { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address1 { get; set; }
+
+        //Does it need this nullable enable line? Creates screen squiggle without it.
+        //#nullable enable
+        public string? Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        //Needs to be an int because most zipcodes consist of 5 digits.
+        public int Zipcode { get; set; }
+
+        public bool Services { get; set; }
+
+        public DateTime AppointmentDate { get; set; }
+
+        /*
         public  int AppointmentId { get; set; }
 
         public DateTime AppointmentDate { get; set; }
@@ -20,6 +56,6 @@ namespace GroomingSalonWebsite.Models
         public override string ToString()
         {
             return AppointmentDate + " for " + ApptName + " and" + ApptPetName ;
-        }
+        }*/
     }
 }
