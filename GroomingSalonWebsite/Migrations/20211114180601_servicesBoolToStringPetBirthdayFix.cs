00305ac7a88316d624adf93309963f3957149dae
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2021-11-14 10:10:35 -0800

    Changed checkboxes to radio for easier picking. Radioboxes display in DB properly. Fixed pet birthday time choice. (#69)

---
+++ GroomingSalonWebsite/Migrations/20211114180601_servicesBoolToStringPetBirthdayFix.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroomingSalonWebsite.Migrations
+{
+    public partial class servicesBoolToStringPetBirthdayFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApptServices",
+                table: "Appointment",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "ApptServices",
+                table: "Appointment",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
