3cc1b6d24eb143abab56bbcd22d832527af1bd2a
Ryder <rdeback@gmail.com>
2021-11-16 15:21:51 -0800

    Made all required fields actually need to be filled out.

--- GroomingSalonWebsite/Models/Appointment.cs
+++ GroomingSalonWebsite/Models/Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,36 +12,48 @@ namespace GroomingSalonWebsite.Models
         [Key]
         public int AppointmentId { get; set; }
 
+        [Required]
         public string ApptFirstName { get; set; }
 
+        [Required]
         public string ApptLastName { get; set; }
 
+        [Required]
         public string ApptPetName { get; set; }
 
+        [Required]
         public string ApptPetBreed { get; set; }
 
         [DataType(DataType.Date)]
+        [Required]
         public DateTime ApptPetBirthDay { get; set; }
 
+        [Required]
         public int ApptPetWeight { get; set; }
 
+        [Required]
         public string ApptPhoneNumber { get; set; }
 
+        [Required]
         public string ApptAddress1 { get; set; }
 
-        //Does it need this nullable enable line? Creates screen squiggle without it.
-        //#nullable enable
+        [Required]
         public string? ApptAddress2 { get; set; }
 
+        [Required]
         public string ApptCity { get; set; }
 
+        [Required]
         public string ApptState { get; set; }
 
         //Needs to be an int because most zipcodes consist of 5 digits.
+        [Required]
         public int ApptZipcode { get; set; }
 
+        [Required]
         public string ApptServices { get; set; }
 
+        [Required]
         public DateTime ApptDate { get; set; }
 
         public override string ToString()
