965502a5661d0c68551952427aacab66553fd2d8
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2021-12-08 15:12:25 -0800

    Changed phoneLength and ran a migration to fix. (#95)

--- GroomingSalonWebsite/Migrations/20211208230936_phoneNumLengthInc.Designer.cs
+++ GroomingSalonWebsite/Migrations/20211208230936_phoneNumLengthInc.Designer.cs
@@ -4,14 +4,16 @@ using GroomingSalonWebsite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GroomingSalonWebsite.Migrations
 {
     [DbContext(typeof(SalonContext))]
-    partial class SalonContextModelSnapshot : ModelSnapshot
+    [Migration("20211208230936_phoneNumLengthInc")]
+    partial class phoneNumLengthInc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GroomingSalonWebsite/Migrations/20211208230936_phoneNumLengthInc.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroomingSalonWebsite.Migrations
+{
+    public partial class phoneNumLengthInc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApptPhoneNumber",
+                table: "Appointment",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApptPhoneNumber",
+                table: "Appointment",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
