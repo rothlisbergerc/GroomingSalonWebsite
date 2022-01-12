be8ea9fcf67658ac5b592a68461d1d6627224639
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2022-01-12 12:54:39 -0800

    Issue#100 validate contact us (#106)
    * Redid the contact us model so that it takes into account what is required now.
    
    * Updated the main page and index to have the proper contactUs info on it.
    
    * Recreated add/update/delete so that they have all necessary information.
    
    * Refactoring the contactUs controller and pages so it will not go to the add page.
    
    * Fixed ID issue on contactus that resulted in null values being passed. Also renamed for inlineCheck to inlineRadio on home.
    
    * Added email validation but put the attribute above wrong section on initial update and migration.

--- GroomingSalonWebsite/Models/ContactUs.cs
+++ GroomingSalonWebsite/Models/ContactUs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,13 +12,26 @@ namespace GroomingSalonWebsite.Models
         [Key]
         public int messageId { get; set; }
         //Name of the customer who sent the message
+        [Required(ErrorMessage ="Please enter your full name")]
+        [DisplayName("Customer Name")]
         public string name { get; set; }
         //The customers email they enter.
+        [Required(ErrorMessage ="Please enter an email")]
+        [EmailAddress]
+        [DisplayName("Customer Email")]
         public string email { get; set; }
         //The main tagline for the email that is being sent
+        [Required(ErrorMessage ="Please enter a title to name your message")]
+        [DisplayName("Title of message")]
         public string subject { get; set; }
         //The actual body of the message for the user to enter their reason for contacting.
+        [Required(ErrorMessage ="Please enter a message to be sent.")]
+        [DisplayName("Message body")]
         public string message { get; set; }
+        //The checkboxes at the bottom of the page weren't being accounted for in the page.
+        [Required(ErrorMessage ="How did you hear about us?")]
+        [DisplayName("Heard about")]
+        public string hearabout { get; set; }
         //Allows the return of all the information of the message in a toString format.
         public override string ToString()
         {
