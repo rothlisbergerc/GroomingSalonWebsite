3f795d0e9943eb8c4cb2abe7ae2f09883b28db1c
Ryder <rdeback@gmail.com>
2022-01-18 16:39:31 -0800

    Added all missing validation to users account class.

---
+++ GroomingSalonWebsite/Migrations/20220119003910_AccountValidationAdded.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroomingSalonWebsite.Migrations
+{
+    public partial class AccountValidationAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
