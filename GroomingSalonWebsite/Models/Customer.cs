95d452fc16db75d79a2eb3a0ccff3bbb99352156
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2022-02-06 11:04:04 -0800

    Updated the documentation of all classes. (#120)

--- GroomingSalonWebsite/Models/Customer.cs
+++ GroomingSalonWebsite/Models/Customer.cs
@@ -2,23 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//Customer class that stores all users information from when they are registering for an appointment.
+/// <summary>
+/// Customer class that stores all users information from when they are registering for an appointment.
+/// </summary>
 namespace GroomingSalonWebsite.Models
 {
     public class Customer
     {
-        //Auto generated id provided by the system
+        /// <summary>
+        /// Auto generated id provided by the system
+        /// </summary>
         public int CustomerId { get; set; }
-        //Customers first name
+        /// <summary>
+        /// Customers first name to be associated with them
+        /// </summary>
         public String FirstName { get; set; }
-        //Customers last name
+        /// <summary>
+        /// Customers last name to be associated with them
+        /// </summary>
         public String LastName { get; set; }
 
-        //Can make this a varchar of length 15 or so for any phone number.
+        /// <summary>
+        /// Customers phone number to be associated with them. Can make this a varchar of length 15 or so for any phone number.
+        /// </summary>
         public String PhoneNumber { get; set; }
-        //The home address of the customer
+        /// <summary>
+        /// The home address of the customer
+        /// </summary>
         public String HomeAddress { get; set; }
-        //Same as pet where if needed it will return an overridden version of toString to display the customer.
+        /// <summary>
+        /// Same as pet where if needed it will return an overridden version of toString to display the customer.
+        /// </summary>
         public override string ToString()
         {
             return FirstName + " " + LastName;
