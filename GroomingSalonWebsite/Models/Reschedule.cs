49b238c219519bb7e0f2807898056553c635ddba
Ryder <rdeback@gmail.com>
2022-02-15 09:20:13 -0800

    Created the reschedule class

---
+++ GroomingSalonWebsite/Models/Reschedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroomingSalonWebsite.Models
+{
+    public class Reschedule
+    {
+        public Appointment Appointment { get; set; }
+
+        public string ButtonState { get; set; }
+
+        public bool popUpConfirm { get; set; }
+    }
+}
